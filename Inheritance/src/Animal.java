a9dd69c1e6fe7566cced198b02d7d4444b216579
Kuel Maluil <kuelmaluil37@gmail.com>
2025-10-15 14:51:48 +0300

    Commit 1
    Int the NV, on line 1, I created class Animal. I created a variable, constructor, method, and its subclass.

---
+++ Inheritance/src/Animal.java
@@ -0,0 +1,25 @@
+public class Animal {
+    protected String name;
+    protected int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void eat() {
+        System.out.println(name + " is eating...");
+    }
+
+    public void sleep() {
+        System.out.println(name + " is sleeping...");
+    }
+
+    public void makeSound() {
+        System.out.println(name + " is making a sound");
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name + ", Age: " + age + " years");
+    }
+}
