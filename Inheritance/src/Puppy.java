e04950865ad291c99652b9e41e701d237397ae4d
Kuel Maluil <kuelmaluil37@gmail.com>
2025-10-15 15:07:33 +0300

    Commit 1
    In the new version, on line 1 inside the class. I created a class Puppy, and the variable, and the constructor, method and subclass have been added.

---
+++ Inheritance/src/Puppy.java
@@ -0,0 +1,26 @@
+public class Puppy extends Dog {
+    private String Toy;
+
+    public Puppy(String name, int age, String breed, String favoriteToy) {
+        super(name, age, breed);
+        this.Toy = Toy;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " the puppy says: Yip Yip!");
+    }
+
+    public void play() {
+        System.out.println(name + " is playing with " + Toy);
+    }
+
+    public void beCute() {
+        System.out.println(name + " is being adorable!");
+    }
+
+    public void displayPuppyInfo() {
+        displayDogInfo();
+        System.out.println("Favorite Toy: " + Toy);
+    }
+}
