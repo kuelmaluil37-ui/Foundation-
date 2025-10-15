a5e64498427721633641eeb291a4eb38f2d3cf92
Kuel Maluil <kuelmaluil37@gmail.com>
2025-10-15 14:57:02 +0300

    Commit 1
    In the new version, on line one inside the class, i have created a class Cat and subclass for variable constructor, method.

---
+++ Inheritance/src/Cat.java
@@ -0,0 +1,25 @@
+public class Cat extends Animal {
+    private String color;
+
+    public Cat(String name, int age, String color) {
+        super(name, age);
+        this.color = color;
+    }
+    
+    public void makeSound() {
+        System.out.println(name + " the " + color + " cat says: Meow Meow!");
+    }
+
+    public void climb() {
+        System.out.println(name + " is climbing a tree");
+    }
+
+    public void purr() {
+        System.out.println(name + " is purring...");
+    }
+
+    public void displayCatInfo() {
+        displayInfo();
+        System.out.println("Color: " + color);
+    }
+}
