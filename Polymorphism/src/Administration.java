f1f59e10b297fdcd5b0512b2e2aee27dd50ed57c
Kuel Maluil <kuelmaluil37@gmail.com>
2025-10-26 15:44:49 +0300

    Commit 1
    In the new version on line 1, I create a class Administration subclass with method overriding.

---
+++ Polymorphism/src/Administration.java
@@ -0,0 +1,29 @@
+
+public class Administration extends University {
+    private String department;
+    private String role;
+
+    public Administration(String name, String id, String department, String role) {
+        super(name, id);
+        this.department = department;
+        this.role = role;
+    }
+
+    @Override
+    public void performAction() {
+        System.out.println("Administration action: " + name + " is managing " + department + " department as " + role);
+    }
+
+    public void manageResources() {
+        super.performAction();
+        System.out.println("Managing university resources efficiently");
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
