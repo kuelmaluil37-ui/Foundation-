dbf8f4ab62648dd54fc5e65d902a6bacf6256c18
Kuel Maluil <kuelmaluil37@gmail.com>
2025-10-26 15:46:51 +0300

    Commit 1
    In the new version on line 1, I created a class Professor subclass with final variables.

---
+++ Polymorphism/src/Professor.java
@@ -0,0 +1,28 @@
+
+public class Professor extends University {
+     String subject;
+     final String EMPLOYMENT_TYPE = "FACULTY";
+
+    public Professor(String name, String id, String subject) {
+        super(name, id);
+        this.subject = subject;
+    }
+
+    @Override
+    public void performAction() {
+        System.out.println("Professor action: " + name + " is teaching " + subject + " to students");
+    }
+
+
+    public void conductResearch() {
+        System.out.println(name + " is conducting research in " + subject);
+    }
+
+    public final String getEmploymentType() {
+        return EMPLOYMENT_TYPE;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+}
