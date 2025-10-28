e543819c748ea57503e395c11fd1146d8bc3eb52
Kuel Maluil <kuelmaluil37@gmail.com>
2025-10-29 00:27:00 +0300

    Commit1
    In the new version on line 1, I created a class Lecturer. I also added lecturer class.
    I added Lecturer class
    - Extends Person with super
    - Implements final employee methods
    - Adds teaching capabilities
    - Demonstrates final method usage

---
+++ Supper/src/Lecturer.java
@@ -0,0 +1,38 @@
+public class Lecturer extends Person {
+    private String department;
+    private final String employeeId;
+    private double salary;
+
+    public Lecturer(String id, String name, String email, String department, String employeeId, double salary) {
+        super(id, name, email);
+        this.department = department;
+        this.employeeId = employeeId;
+        this.salary = salary;
+    }
+
+    public final String getEmployeeId() {
+        return employeeId;
+    }
+
+    @Override
+    public void displayBasicInfo() {
+        super.displayBasicInfo();
+        System.out.println("Department: " + department + ", Employee ID: " + employeeId);
+    }
+
+    public void conductLecture(String courseName) {
+        System.out.println(getName() + " is conducting lecture for: " + courseName);
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
