9a6f6c8a23dc9fed1bbe73d245075ab609ac895b
Kuel Maluil <kuelmaluil37@gmail.com>
2025-10-29 00:17:17 +0300

    Commit1
    In the new version on line 1, I created Person. I also added Person base class
    
    - Final class that cannot be extended
    - Final variables for immutable properties
    - Final methods that cannot be overridden
    - Base for all university members"

---
+++ Supper/src/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    private final String id;
+    private final String name;
+    private final String email;
+
+    public Person(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void displayBasicInfo() {
+        System.out.println("ID: " + id + ", Name: " + name + ", Email: " + email);
+    }
+}
