0034210712ce326d951722308a42669806eeb090
Kuel Maluil <kuelmaluil37@gmail.com>
2025-10-29 00:32:40 +0300

    Commit1
    I created Course class
    - Final course code and name variables
    - Course-instructor assignment
    - Credit management system
    - Immutable course properties

---
+++ Supper/src/Course.java
@@ -0,0 +1,40 @@
+public class Course {
+    private final String courseCode;
+    private final String courseName;
+    private Lecturer instructor;
+    private int credits;
+
+    public Course(String courseCode, String courseName, int credits) {
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+        this.credits = credits;
+    }
+
+    public final String getCourseCode() {
+        return courseCode;
+    }
+
+    public final String getCourseName() {
+        return courseName;
+    }
+
+    public void assignInstructor(Lecturer instructor) {
+        this.instructor = instructor;
+        System.out.println("Instructor " + instructor.getName() + " assigned to course " + courseName);
+    }
+
+    public void displayCourseInfo() {
+        System.out.println("Course: " + courseCode + " - " + courseName + " (" + credits + " credits)");
+        if (instructor != null) {
+            System.out.println("Instructor: " + instructor.getName());
+        }
+    }
+
+    public Lecturer getInstructor() {
+        return instructor;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+}
