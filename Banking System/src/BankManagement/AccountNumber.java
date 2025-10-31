259f338c584efbdada1bbd4bd72739fdede49f52
Kuel Maluil <kuelmaluil37@gmail.com>
2025-10-31 13:08:28 +0300

    Commit1
    In the new version on line 3, I created a class AccountNumber.

---
+++ Banking System/src/BankManagement/AccountNumber.java
@@ -0,0 +1,17 @@
+package BankManagement;
+
+public class AccountNumber {
+    private static long lastAccountNumber = 005000000070L;
+
+    public static synchronized long AccountNumber() {
+        return ++lastAccountNumber;
+    }
+
+    public static long getLastGeneratedAccountNumber() {
+        return lastAccountNumber;
+    }
+
+    public static synchronized void reset() {
+        lastAccountNumber = 005000000070L;
+    }
+}
