375e45d2690d2f27d442ad08bb5f0f32063319fa
Kuel Maluil <kuelmaluil37@gmail.com>
2025-10-31 13:51:25 +0300

    Commit 4
    In the new version on line 6, I updated an Account to public static synchronized long AccountNumber.

--- Banking System/src/BankManagement/AccountNumber.java
+++ Banking System/src/BankManagement/AccountNumber.java
@@ -3,7 +3,7 @@ package BankManagement;
 public class AccountNumber {
     private static long lastAccountNumber = 005000000070L;
 
-    public static synchronized long AccountNumber() {
+    public static synchronized long AccountNumber(Account) {
         return ++lastAccountNumber;
     }
 
