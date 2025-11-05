7f788d82ff2b6e120678b55732e7424b41a97849
Kuel Maluil <kuelmaluil37@gmail.com>
2025-11-05 20:07:49 +0300

    Commit 1
    In the new version on line 1, I created class called Main instance variables, a constructor and display the method.

---
+++ Testing/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        Vehicle car = new Car("Toyota", "Camry", 2023, 25000, 4, "Gasoline");
+        Vehicle motorcycle = new Motorcycle("Harley-Davidson", "Sportster", 2023, 12000, "Cruiser", 1200);
+        Vehicle truck = new Truck("Ford", "F-150", 2023, 35000, 1.5, 2);
+
+        System.out.println("=== VEHICLE DETAILS ===");
+        car.displayDetails();
+        System.out.println("Current value after 2 years: $" + car.computeCost(2));
+        System.out.println();
+
+        motorcycle.displayDetails();
+        System.out.println("Current value after 2 years: $" + motorcycle.computeCost(2));
+        System.out.println();
+
+        truck.displayDetails();
+        System.out.println("Current value after 2 years: $" + truck.computeCost(2));
+        System.out.println();
+
+        System.out.println("=== POLYMORPHISM DEMONSTRATION ===");
+        Vehicle[] vehicles = {car, motorcycle, truck};
+
+        for (Vehicle vehicle : vehicles) {
+            vehicle.displayDetails();
+            System.out.println("3-year depreciation value: $" + vehicle.computeCost(3));
+            System.out.println("-------------------");
+        }
+    }
+}
