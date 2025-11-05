3232f72b20db161ef9f37176de9a9645fdc0ad09
Kuel Maluil <kuelmaluil37@gmail.com>
2025-11-05 20:17:04 +0300

    Commit 1
    In the new version on line 1, I created class car extends to Vehicle with 2 protected instance variables, a constructor and display the method.

---
+++ Testing/src/Car.java
@@ -0,0 +1,22 @@
+public class Car extends Vehicle {
+    private int numDoors;
+    private String fuelType;
+
+    public Car(String make, String model, int year, double basePrice,
+               int numDoors, String fuelType) {
+        super(make, model, year, basePrice);
+        this.numDoors = numDoors;
+        this.fuelType = fuelType;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Car Details:");
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+        System.out.println("Doors: " + numDoors);
+        System.out.println("Fuel Type: " + fuelType);
+        System.out.println("Base Price: $" + basePrice);
+    }
+}
